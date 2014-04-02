467720ce86ef511017d67cf6ef105716614d7597
Janette Day <janette@nuams.com>
2014-04-01 20:58:58 -0700

    Updating default hero image path

--- theme/system/page.tpl.php
+++ theme/system/page.tpl.php
@@ -80,27 +80,24 @@
 
 <?php
 if ($is_front):
-$uri = theme_get_setting('hero_path');
-$path = file_create_url($uri);
-?>
-<!-- #jumbotron -->
-<div class="region-jumbotron clearfix" <?php print 'style="background-image:url(' . $path . ');"' ?>>
-  <div class="tint"></div>
-  <!-- jumbotron-inside -->
-  <div class="jumbotron-inside container clearfix">
-    <?php if ($page['preface']): ?>
-        <?php print render($page['preface']); ?>
-    <?php endif; ?>
-    <div class="row">
-      <?php if ($page['preface_first']): ?>
-        <?php print render($page['preface_first']); ?>
-      <?php endif; ?>
-      <?php if ($page['preface_second']): ?>
-        <?php print render($page['preface_second']); ?>
-      <?php endif; ?>
+  $uri = theme_get_setting('hero_path');
+  $path = file_create_url($uri);
+  ?>
+  <!-- #jumbotron -->
+  <div class="region-jumbotron clearfix" <?php print 'style="background-image:url(' . $path . ');"' ?>>
+    <div class="tint"></div>
+    <!-- jumbotron-inside -->
+    <div class="jumbotron-inside container clearfix">
+      <div class="row">
+        <?php if ($page['preface_first']): ?>
+          <?php print render($page['preface_first']); ?>
+        <?php endif; ?>
+        <?php if ($page['preface_second']): ?>
+          <?php print render($page['preface_second']); ?>
+        <?php endif; ?>
+      </div>
     </div>
   </div>
-</div>
 <?php endif; ?>
 
 <div id="page-wrapper">
